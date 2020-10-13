cfee17010e4031bee3eecca9f71b227f5e725d75
mcaniot <mcaniot@aldebaran.com>
2020-10-13 15:56:39 +0200

    drop last in trainloader and reduce batch_size Signed-off-by: mcaniot <mcaniot@aldebaran.com>

--- script/main.py
+++ script/main.py
@@ -8,13 +8,13 @@ from torch import nn, optim
 import data_augmentation
 from simclr_framework import SimCLRFramework
 
-BATCH_SIZE = 256
+BATCH_SIZE = 128
 
 trainset = datasets.STL10(
     '../data/', download=False, split="unlabeled",
     transform=data_augmentation.CreatePosPair(96))
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=BATCH_SIZE, shuffle=True)
+    trainset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True) 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 framework = SimCLRFramework(trainloader, BATCH_SIZE, device)
 framework.train()
